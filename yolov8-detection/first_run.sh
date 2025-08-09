7e41d9f280953bd20e1cd35b7a9ff4a49064a8c9
Sunil Jain <sjain@apptech.insure>
2025-08-09 17:16:21 -0400

    docker module requirements

---
+++ yolov8-detection/first_run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Pull the docker image from the registry
+docker pull sjainapptech/yolov8-detection-module:latest
+
+# Exit with a success code
+exit 0
---
+++ yolov8-detection/start_module.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# The last argument passed to this script is the path to the socket
+# Viam provides this automatically
+SOCKET_PATH="${@: -1}"
+
+# Run the docker container
+# --gpus all: This passes the GPU from the host to the container
+# -v /path/to/your/models:/models: This is an example of how you could mount a model directory
+#    from the host into the container. You would need to create this directory on your robot
+#    and place your .pt file there. Then, in the Viam config, you would set the
+#    "model_location" attribute to "/models/your_model.pt".
+# -v ${SOCKET_PATH}:/tmp/module.sock: This mounts the Viam socket into the container
+# The final part is the image name and the command to run inside it, passing the socket path.
+exec docker run \
+    --gpus all \
+    -v ${SOCKET_PATH}:/tmp/module.sock \
+    sjainapptech/yolov8-detection-module:latest \
+    /usr/bin/python3 /app/src/main.py --socket-path /tmp/module.sock
