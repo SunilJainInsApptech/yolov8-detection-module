fb84c6c0c2261e01de3f6f1fdcd351b9a2f4221d
Sunil Jain <sjain@apptech.insure>
2025-08-10 14:25:41 -0400

    start.sh gpu flag fix

--- yolov8-detection/start_module.sh
+++ yolov8-detection/start_module.sh
@@ -5,7 +5,7 @@
 SOCKET_PATH="${@: -1}"
 
 # Run the docker container
-# --gpus all: This passes the GPU from the host to the container
+# --runtime=nvidia: This uses the NVIDIA container runtime to pass the GPU into the container.
 # -v /path/to/your/models:/models: This is an example of how you could mount a model directory
 #    from the host into the container. You would need to create this directory on your robot
 #    and place your .pt file there. Then, in the Viam config, you would set the
@@ -13,7 +13,7 @@ SOCKET_PATH="${@: -1}"
 # -v ${SOCKET_PATH}:/tmp/module.sock: This mounts the Viam socket into the container
 # The final part is the image name and the command to run inside it, passing the socket path.
 exec docker run \
-    --gpus all \
+    --runtime=nvidia \
     -v ${SOCKET_PATH}:/tmp/module.sock \
     sjainapptech/yolov8-detection-module:latest \
     /usr/bin/python3 /app/src/main.py --socket-path /tmp/module.sock
