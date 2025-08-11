ce488c01bab1cd6413d806cbeb01e8501ed0a8d9
Sunil Jain <sjain@apptech.insure>
2025-08-11 10:03:19 -0400

    start_module.sh fix

--- yolov8-detection/start_module.sh
+++ yolov8-detection/start_module.sh
@@ -4,15 +4,18 @@
 # Viam provides this automatically
 SOCKET_PATH="${@: -1}"
 
-# Run the docker container
+# Run the docker container with flags recommended by NVIDIA for PyTorch
+# --gpus all: Exposes all available GPUs to the container.
+# --ipc=host: Use the host's shared memory, crucial for multi-process data loading in PyTorch.
+# --ulimit...: Increase memory limits for performance.
 # --runtime=nvidia: This uses the NVIDIA container runtime to pass the GPU into the container.
-# -v /path/to/your/models:/models: This is an example of how you could mount a model directory
-#    from the host into the container. You would need to create this directory on your robot
-#    and place your .pt file there. Then, in the Viam config, you would set the
-#    "model_location" attribute to "/models/your_model.pt".
 # -v ${SOCKET_PATH}:/tmp/module.sock: This mounts the Viam socket into the container
 # The final part is the image name and the command to run inside it, passing the socket path.
 exec docker run \
+    --gpus all \
+    --ipc=host \
+    --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
     --runtime=nvidia \
     -v ${SOCKET_PATH}:/tmp/module.sock \
     sjainapptech/yolov8-detection-module:latest \
