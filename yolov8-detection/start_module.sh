e3cb410f9a1c03a0e8c42b18f1486817e12aa3ed
Sunil Jain <sjain@apptech.insure>
2025-08-11 10:16:20 -0400

    --rm to start_module

--- yolov8-detection/start_module.sh
+++ yolov8-detection/start_module.sh
@@ -5,6 +5,7 @@
 SOCKET_PATH="${@: -1}"
 
 # Run the docker container with flags recommended by NVIDIA for PyTorch
+# --rm: Automatically remove the container when it exits. Prevents "address already in use" errors.
 # --gpus all: Exposes all available GPUs to the container.
 # --ipc=host: Use the host's shared memory, crucial for multi-process data loading in PyTorch.
 # --ulimit...: Increase memory limits for performance.
@@ -12,6 +13,7 @@ SOCKET_PATH="${@: -1}"
 # -v ${SOCKET_PATH}:/tmp/module.sock: This mounts the Viam socket into the container
 # The final part is the image name and the command to run inside it, passing the socket path.
 exec docker run \
+    --rm \
     --gpus all \
     --ipc=host \
     --ulimit memlock=-1 \
