dadd353d8bafd300d7de804f41efc44f639a6b34
Sunil Jain <sjain@apptech.insure>
2025-08-11 11:15:08 -0400

    debug to cpu

--- yolov8-detection/start_module.sh
+++ yolov8-detection/start_module.sh
@@ -4,17 +4,13 @@
 # Viam provides this automatically
 SOCKET_PATH="${@: -1}"
 
-# Run the docker container with flags recommended by NVIDIA for PyTorch
+# Run the docker container without GPU for initial testing
 # --rm: Automatically remove the container when it exits.
 # --ipc=host: Use the host's shared memory.
-# --runtime=nvidia: Use the NVIDIA container runtime to pass the GPU into the container.
-# --privileged: Grant the container extended privileges to access host devices (like the GPU).
 # -v ${SOCKET_PATH}:/tmp/module.sock: Mount the Viam socket into the container.
 exec docker run \
     --rm \
     --ipc=host \
-    --runtime=nvidia \
-    --privileged \
     -v ${SOCKET_PATH}:/tmp/module.sock \
     sjainapptech/yolov8-detection-module:latest \
     /usr/bin/python3 /app/src/main.py --socket-path /tmp/module.sock
