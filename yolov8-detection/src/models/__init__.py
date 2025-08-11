2b00ce31582ad68f7f77e3d7fb58865981b9e71a
Sunil Jain <sjain@apptech.insure>
2025-08-11 19:50:56 -0400

    jjj

--- yolov8-detection/src/models/__init__.py
+++ yolov8-detection/src/models/__init__.py
@@ -3,11 +3,3 @@ from viam.services.vision import Vision
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from .yolov8_detection import YOLOv8DetectionService
 
-Registry.register_resource_creator(
-	Vision.API,
-	YOLOv8DetectionService.MODEL,
-	ResourceCreatorRegistration(
-		YOLOv8DetectionService.new,
-		YOLOv8DetectionService.validate_config
-	)
-)
